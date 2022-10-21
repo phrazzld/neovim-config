7c720de2bf96bacc23ce23d26dac09f5f6c9b573
Phaedrus Raznikov <praznikov@cerego.com>
2022-10-21 16:56:42 -0400

    Add MarkdownPreview plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -136,6 +136,7 @@ return packer.startup(function(use)
   use("junegunn/goyo.vim")
   use({ "ellisonleao/glow.nvim", branch = "main" })
   use("preservim/vim-markdown")
+  use({ "iamcco/markdown-preview.nvim", run = "cd app && yarn install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
   -- diagnostics management
   use({
