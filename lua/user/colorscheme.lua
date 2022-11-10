5c64fb029e1428c98adc21f68e8e9b0dd96133b5
Phaedrus Raznikov <phaedrus.raznikov@memre.ai>
2022-11-10 17:33:15 -0600

    Tweak colorschemes

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,13 +1,14 @@
 vim.opt.termguicolors = true
 
-local colorscheme = "tokyonight"
---[[ local colorscheme = "gruvbox" ]]
+local colorscheme
 
--- Use dark mode from 1800 - 0700
+-- Use dark mode from 1700 - 0800
 local hour = os.date("*t").hour
-if hour > 17 or hour < 7 then
+if hour >= 17 or hour <= 7 then
+	colorscheme = "tokyonight"
 	vim.opt.background = "dark"
 else
+	colorscheme = "github_light"
 	vim.opt.background = "light"
 end
 
