979f44cb6536dc0997cc23e040cce24932ce6dd9
phaedrus <phaedrus@reacher.me>
2023-07-15 13:44:54 -0700

    tweak lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -68,7 +68,7 @@ lualine.setup({
 		theme = "OceanicNext",
 		component_separators = { left = '', right = '' },
 		section_separators = { left = '', right = '' },
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+		disabled_filetypes = { "alpha", "dashboard", "Outline" },
 		always_divide_middle = false,
 	},
 	sections = {
@@ -88,5 +88,5 @@ lualine.setup({
 		lualine_z = {},
 	},
 	tabline = {},
-	extensions = {},
+	extensions = {'fugitive', 'nvim-tree', 'toggleterm', 'trouble'},
 })
