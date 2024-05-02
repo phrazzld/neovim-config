042bd293c52bfd2d732efaf7f318080abcafc123
phaedrus <phaedrus@reacher.me>
2024-05-02 11:00:59 -0500

    feat(colors): random

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,58 @@
+--vim.opt.termguicolors = true
+
+-- function to determine whether it's currently day or night
+--local function is_daytime()
+--	local hour = os.date("*t").hour
+--	return hour < 19 and hour > 6
+--end
+--
+--local colorscheme
+--if is_daytime() then
+--	vim.opt.background = "light"
+--	colorscheme = "kanagawa"
+--else
+--	vim.opt.background = "dark"
+--	colorscheme = "rose-pine"
+--end
+
+--[[ vim.opt.background = "dark" ]]
+--[[ local colorscheme = "rose-pine" ]]
+--[[ vim.opt.background = "light" ]]
+--[[ local colorscheme = "rose-pine-dawn" ]]
+
+--local ok, err = pcall(vim.cmd, "colorscheme " .. colorscheme)
+--
+--if not ok then
+--	vim.notify("Failed to load the colorscheme '" .. colorscheme .. "'. Error: " .. err)
+--	return
+--end
+
+-- good light themes
+-- - everforest
+-- - github_light
+-- - kanagawa
+-- - lunaperche
+-- - wildcharm
+-- - rose-pine
+
 vim.opt.termguicolors = true
+
+-- Seed the random generator
+math.randomseed(os.time())
+
+-- Arrays of color schemes for light and dark modes
+local light_colorschemes = {
+	"everforest",
+	"github_light",
+	"kanagawa",
+	"lunaperche",
+	"wildcharm",
+	"rose-pine",
+	"tokyonight-day",
+	"dayfox",
+	"gruvbox",
+}
+local dark_colorschemes = { "rose-pine", "tokyonight", "gruvbox", "kanagawa", "github_dark", "nightfox" }
 
 -- function to determine whether it's currently day or night
 local function is_daytime()
@@ -7,24 +61,20 @@ local function is_daytime()
 end
 
 local colorscheme
+
 if is_daytime() then
 	vim.opt.background = "light"
-	colorscheme = "github_light"
+	-- Select a random light colorscheme
+	colorscheme = light_colorschemes[math.random(#light_colorschemes)]
 else
 	vim.opt.background = "dark"
-	colorscheme = "rose-pine"
+	-- Select a random dark colorscheme
+	colorscheme = dark_colorschemes[math.random(#dark_colorschemes)]
 end
 
 local ok, err = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not ok then
-	vim.notify("Failed to load the colorscheme '" .. colorscheme .. "'. Error: " .. err)
+	vim.notify("Failed to load the colorscheme '" .. colorscheme .. "'. Error: " .. err, vim.log.levels.ERROR)
 	return
 end
-
--- good light themes
--- - everforest
--- - github_light
--- - kanagawa
--- - lunaperche
--- - wildcharm
