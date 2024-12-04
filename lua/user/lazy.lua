9defff708d0886712d47c71b806048072245cfa1
phaedrus <phaedrus@reacher.me>
2024-12-04 10:32:57 -0600

    feat(plugins): trouble and todo-comments

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -139,6 +139,57 @@ require("lazy").setup({
 
 		-- make it easy to add surrounding characters
 		{ "tpope/vim-surround" },
+
+		-- todos and diagnostics
+		{
+			"folke/trouble.nvim",
+			opts = {}, -- for default options, refer to the configuration section for custom setup.
+			cmd = "Trouble",
+			keys = {
+				{
+					"<leader>xx",
+					"<cmd>Trouble diagnostics toggle<cr>",
+					desc = "Diagnostics (Trouble)",
+				},
+				{
+					"<leader>xX",
+					"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+					desc = "Buffer Diagnostics (Trouble)",
+				},
+				{
+					"<leader>cs",
+					"<cmd>Trouble symbols toggle focus=false<cr>",
+					desc = "Symbols (Trouble)",
+				},
+				{
+					"<leader>cl",
+					"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+					desc = "LSP Definitions / references / ... (Trouble)",
+				},
+				{
+					"<leader>xL",
+					"<cmd>Trouble loclist toggle<cr>",
+					desc = "Location List (Trouble)",
+				},
+				{
+					"<leader>xQ",
+					"<cmd>Trouble qflist toggle<cr>",
+					desc = "Quickfix List (Trouble)",
+				},
+			},
+		},
+		{
+			"folke/todo-comments.nvim",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			opts = {},
+			keys = {
+				{
+					"<leader>xt",
+					"<cmd>TodoQuickFix<cr>",
+					desc = "Todos (TodoTelescope)",
+				},
+			},
+		},
 	},
 	-- configure any other settings here
 	-- colorscheme that will be used when installing plugins
